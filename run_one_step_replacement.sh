44442757edf010a27ecd8b46384c08992707a18a
gsd <gsd2003_20@163.com>
2025-12-09 08:39:59 +0000

    final

--- run_one_step_replacement.sh
+++
@@ -1,6 +0,0 @@
-python scripts/run_one_step_replacement.py \
-  --sims 25 \
-  --num-repeat 32 \
-  --after-policy baseline \
-  --data-dir ./robust_vs_baseline_data \
-  --out-dir ./robust_vs_baseline_data
--- train.sh
+++
@@ -1,4 +0,0 @@
-# nohup env TQDM_DISABLE=1 python main.py --device 0 --no-use-dyn-c --no-use-sym --checkpoint ./baseline > baseline.log 2>&1 &
-# nohup env TQDM_DISABLE=1 python main.py --device 1 --use-dyn-c --no-use-sym --checkpoint ./diy_dyn > diy_dyn.log 2>&1 &
-nohup env TQDM_DISABLE=1 python main.py --device 0 --no-use-dyn-c --no-use-sym --checkpoint ./teacher > teacher.log 2>&1 &
-# nohup env TQDM_DISABLE=1 python main.py --device 3 --use-dyn-c --use-sym --checkpoint ./diy_dyn_sym > diy_dyn_sym.log 2>&1 &
